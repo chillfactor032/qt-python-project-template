3bd27a1c6f7d8ad6c10a5f6a57aa64af0cf38373
chillfactor032 <chill@chillaspect.com>
2022-08-02 17:02:09 -0500

    Initial Commit
    Creating project template

---
+++ MainWindow.py
@@ -0,0 +1,97 @@
+# Python Imports
+import sys
+import json
+import os
+from enum import Enum
+
+# PySide6 Imports
+from PySide6.QtWidgets import QApplication, QMainWindow, QStyle
+from PySide6.QtCore import QSettings, QFile, QTextStream, QStandardPaths
+from PySide6.QtGui import QPixmap, QIcon
+import Resources
+from UI_Components import Ui_MainWindow
+
+#Log Levels
+class LogLevel(Enum):
+    INFO = 0
+    ERROR = 10
+    DEBUG = 20
+    
+    @staticmethod
+    def get(value):
+        for level in LogLevel:
+            if(value == level.value):
+                return level
+        return LogLevel.INFO
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        #Load UI Components
+        self.setupUi(self)
+        
+        #Read Version File From Resources
+        version_file = QFile(":version.json")
+        version_file.open(QFile.ReadOnly)
+        text_stream = QTextStream(version_file)
+        version_file_text = text_stream.readAll()
+        self.version_dict = json.loads(version_file_text)
+        self.app_name = self.version_dict["product_name"]
+        self.version = self.version_dict["version"]
+        self.project_name = self.app_name.title().replace(" ", "")
+        self.setWindowTitle(f"{self.app_name} {self.version}")
+        
+        #Load Settings
+        self.config_dir = QStandardPaths.writableLocation(QStandardPaths.ConfigLocation)
+        if(not os.path.isdir(self.config_dir)):
+            os.mkdir(self.config_dir)
+        self.ini_path = os.path.join(self.config_dir, f"{self.project_name}.ini").replace("\\", "/")
+        self.settings = QSettings(self.ini_path, QSettings.IniFormat)
+
+        #Set window Icon
+        default_icon_pixmap = QStyle.StandardPixmap.SP_FileDialogListView
+        pc_icon_pixmap = QPixmap(":resources/img/pc_icon.ico")
+        pc_icon = QIcon(pc_icon_pixmap)
+        default_icon = self.style().standardIcon(default_icon_pixmap)
+        if(pc_icon):
+            self.setWindowIcon(pc_icon)
+        else:
+            self.setWindowIcon(default_icon)
+        
+        #Setup Button Signals
+        # Button/Menu Signals Go Here
+        
+        #Finally, Show the UI
+        geometry = self.settings.value(f"{self.project_name}/geometry")
+        window_state = self.settings.value(f"{self.project_name}/windowState")
+        if(geometry and window_state):
+            self.restoreGeometry(geometry) 
+            self.restoreState(window_state)
+        self.show()
+
+    # App is closing, cleanup
+    def closeEvent(self, evt):
+        # Remember the size and position of the GUI
+        self.settings.setValue(f"{self.project_name}/geometry", self.saveGeometry())
+        self.settings.setValue(f"{self.project_name}/windowState", self.saveState())
+        self.settings.sync()
+        evt.accept()
+
+# Start the PySide6 App
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    version_file = QFile(":version.json")
+    version_file.open(QFile.ReadOnly)
+    text_stream = QTextStream(version_file)
+    version_file_text = text_stream.readAll()
+    version_dict = json.loads(version_file_text)
+    org_name = version_dict["company_name"]
+    app_name = version_dict["product_name"]
+    version = version_dict["version"]
+    app.setOrganizationName(org_name)
+    app.setApplicationName(app_name)
+    app.setApplicationVersion(version)
+    window = MainWindow()
+    sys.exit(app.exec())
---
+++ build.py
@@ -0,0 +1,254 @@
+import os
+import subprocess
+import shutil
+import sys
+import json
+
+
+VERSION_TEMPLATE = """
+{
+    "version": "<VERSION>",
+    "ico": "./resources/img/<ICON>.png",
+    "company_name": "<COMPANY_NAME>",
+    "product_name": "<PROJECT_NAME>",
+    "description": "<PROJECT_DESCRIPTION>"
+}
+"""
+
+RESOURCES_TEMPLATE = """
+<!DOCTYPE RCC><RCC version="1.0">
+<qresource>
+    <file>version.json</file>
+</qresource>
+</RCC>
+"""
+
+UI_TEMPPLATE = """
+<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>MainWindow</class>
+ <widget class="QMainWindow" name="MainWindow">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>640</width>
+    <height>480</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>MainWindow</string>
+  </property>
+  <widget class="QWidget" name="centralwidget">
+   <widget class="QLabel" name="label">
+    <property name="geometry">
+     <rect>
+      <x>40</x>
+      <y>20</y>
+      <width>441</width>
+      <height>16</height>
+     </rect>
+    </property>
+    <property name="text">
+     <string>UI Template - Replace this file with one from QT Designer</string>
+    </property>
+   </widget>
+  </widget>
+  <widget class="QMenuBar" name="menubar">
+   <property name="geometry">
+    <rect>
+     <x>0</x>
+     <y>0</y>
+     <width>640</width>
+     <height>22</height>
+    </rect>
+   </property>
+  </widget>
+  <widget class="QStatusBar" name="statusbar"/>
+ </widget>
+ <resources/>
+ <connections/>
+</ui>
+"""
+
+target_env = "windows"
+partial = False
+
+if(len(sys.argv)>1):
+    if(sys.argv[1] == "partial"):
+        partial = True
+
+print("Starting build script")
+if not os.path.exists("version.json"):
+    print("version.json file does not exist. Generating template")
+    print("Complete this template before rerunning")
+    with open("version.json", "w") as f:
+        f.write(VERSION_TEMPLATE)
+    sys.exit(1)
+
+print("Reading Version File: version.json")
+with open("version.json", "r") as version_file:
+    version = json.load(version_file)
+print(f"Company Name: {version['company_name']}")
+print(f"Product Name: {version['product_name']}")
+print(f"Version: {version['version']}")
+print()
+
+TEST_BUILD = False
+PROJECT_NAME = version['product_name'].title().replace(" ","")
+OUTPUT_FILE = f"{PROJECT_NAME}.exe"
+# Specify Paths and Files
+cwd = os.getcwd()
+ui_path = os.path.join(cwd, "resources", "ui")
+destination_file = os.path.join(cwd, "UI_Components.py")
+resource_file = os.path.join(cwd, f"{PROJECT_NAME}.rc")
+
+try:
+    if not os.path.exists("resources"):
+        print("Creating resources directory")
+        os.mkdirs("bin")
+except Exception as e:
+    print("Error creating bin dir")
+    print(e)
+
+def make_dirs(path):
+    try:
+        if not os.path.exists(path):
+            print(f"Creating path {path}")
+            os.mkdirs(path)
+    except Exception as e:
+        print("Error creating bin dir")
+        print(e)
+
+# Function to find all files in dir_path 
+# with extension (not recursive)
+def getFilesWithExtension(dir_path,  extension):
+    files = []
+    for file in os.listdir(dir_path):
+        if file.endswith(f".{extension}"):
+            files.append(os.path.join(ui_path, file))
+    return files
+    
+# Function to compile the provided ui_file to py
+# and place it in destination_path
+def compileUiFile(ui_file, destination_file):
+    # uic -g python $ui_file >> destination_file
+    ret = subprocess.run(["uic", "-g", "python", ui_file], capture_output=True)
+    if(ret.returncode != 0):
+        print(f"\nError Compiling {ui_file}")
+        print(ret.stderr)
+        return False
+    output = ret.stdout
+    with open(destination_file, "ab") as py_file:
+        py_file.write(output)
+        py_file.write(b"\r\n\r\n")
+    return True
+
+# Function to compile the provided ui_file to py
+# and place it in destination_path
+def compileResources(resources_file, destination_file):
+    if not os.path.exists(destination_file):
+        #Create the destination file if it doesnt exist
+        with open(destination_file, 'w') as fp:
+            pass
+
+    #rcc -g python -o Resources.py {PROJECT_NAME}.rc
+    ret = subprocess.run(["rcc", "-g", "python", "-o", destination_file, resources_file], capture_output=True)
+    stderr = ret.stderr.decode("utf-8")
+    if(ret.returncode != 0):
+        print(stderr)
+        return False
+    return True
+
+# Make sure resources directories exist, create them if not
+make_dirs("resources/ui")
+make_dirs("resources/files")
+make_dirs("resources/img")
+make_dirs("bin")
+
+print("Compiling All UI Files to Single Python File")
+print(f"UI File Dir: {ui_path}")
+print(f"Destination Python File: {destination_file}")
+
+if(os.path.exists(destination_file)):
+    print("Existing Destination File Found")
+    destination_file_bak = destination_file + ".bak"
+    print(f"Making Backup: {destination_file_bak}")
+    try:
+        shutil.copyfile(destination_file, destination_file_bak)
+        print("\tFile Backup Success")
+    except Exception as e:
+        print("Error Making Backup File")
+        print(e)
+        print("Quitting...")
+        sys.exit(1)
+    print("Removing Old Destination File")
+    os.remove(destination_file)
+print("=============================================\n")
+
+ui_files = getFilesWithExtension(ui_path, "ui")
+#If no ui files, create the blank one
+if len(ui_files) == 0:
+    with open("template.ui") as f:
+        f.write(UI_TEMPPLATE)
+    ui_files = ["template.ui"]
+
+for file in ui_files:
+    print(f"Compiling {file}...", end="")
+    if(compileUiFile(file, destination_file)):
+        print("Success")
+    else:
+        print("Failure")
+
+print("\nUI Files Compiled")
+print("=============================================\n")
+print("Compiling Resources")
+
+#Create Resources.rc if it doesn't exist
+if not os.path.exists("Resources.rc"):
+    print("Resources.rc does not exist. Creating it")
+    with open("Resources.rc", "w") as f:
+        f.write(RESOURCES_TEMPLATE)
+
+resource_dest = "Resources.py"
+if(compileResources("Resources.rc", resource_dest)):
+    print(f"Compiling Resources File: Resources.rc to {resource_dest}")
+else:
+    print(f"\nError Compiling {resource_file}")
+
+print("=============================================\n")
+
+if(partial):
+    print("Partial Build Requested. Not Compiling Binary")
+    print("=============================================\n")
+    sys.exit(0)
+
+print("Compiling Binary")
+
+if(target_env == "windows"):
+    show_cmd = "--windows-disable-console "
+    if(TEST_BUILD):
+        show_cmd = ""
+        
+    cmd = f"py -m nuitka --onefile --standalone " \
+            f"--enable-plugin=pyside6 --windows-icon-from-ico={version['ico']} " \
+            f"{show_cmd}" \
+            f" -o bin/{OUTPUT_FILE} " \
+            "MainWindow.py"
+    
+proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+for c in iter(lambda: proc.stdout.read(1), b''):
+    sys.stdout.write(c.decode("utf-8"))
+    
+for c in iter(lambda: proc.stderr.read(1), b''):
+    sys.stdout.write(c.decode("utf-8"))
+
+proc.communicate()
+
+print("\n=============================================")
+if(proc.returncode == 0):
+    print("Binary Compiled Successfully")
+    print(f"./bin/{OUTPUT_FILE}")
+else:
+    print("Error Compiling Binary")
+print("=============================================\n")
